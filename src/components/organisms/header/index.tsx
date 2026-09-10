480581b64cca3b181b05ecf7ad29bf3a87edbe81
BrenoLinux <breno_tosi@hotmail.com>
2026-09-10 14:31:53 -0300

    feat: improve mobile responsiveness in Header component with updated styles and layout adjustments

--- src/components/organisms/header/index.tsx
+++ src/components/organisms/header/index.tsx
@@ -9,20 +9,20 @@ export const Header = (): ReactNode => {
       className={clsx(
         "relative isolate flex min-h-screen flex-col justify-center gap-[2.4rem] px-60",
         "max-desktop1024:px-24 overflow-hidden",
-        "max-mobile:gap-[1.6rem] max-mobile:px-[3.2rem]",
+        "max-mobile:min-h-0 max-mobile:justify-start max-mobile:gap-6 max-mobile:px-[3.2rem] max-mobile:pt-128 max-mobile:pb-192",
         "before:absolute before:right-0 before:z-[-1] before:h-[94.1rem] before:w-[126.4rem] before:bg-[url('/header/grafismo.png')] before:bg-cover before:bg-center before:bg-no-repeat before:opacity-100 before:content-['']",
-        "max-desktop1024:before:left-[48%] max-desktop1024:before:right-auto max-desktop1024:before:h-[70rem] max-desktop1024:before:w-[62%] max-desktop1024:before:bg-contain",
-        "max-mobile:before:left-[45%] max-mobile:before:h-[58rem] max-mobile:before:w-[80%] max-mobile:before:opacity-50",
+        "max-desktop1024:before:left-[48%] max-desktop1024:before:right-auto max-desktop1024:before:h-280 max-desktop1024:before:w-[62%] max-desktop1024:before:bg-contain",
+        "max-mobile:before:bottom-0 max-mobile:before:left-auto max-mobile:before:right-[-8%] max-mobile:before:h-168 max-mobile:before:w-[90%] max-mobile:before:bg-bottom-right max-mobile:before:opacity-80",
       )}
     >
       <h1
         className={clsx(
           "relative font-cormorant text-[16rem] font-semi-bold leading-none tracking-[0.3em] text-dark-blue",
           "max-desktop1024:text-[12rem]",
-          "max-mobile:text-[8rem] max-mobile:tracking-[0.2em]",
+          "max-mobile:pb-6 max-mobile:text-[10rem] max-mobile:tracking-[0.18em]",
           "before:absolute before:bottom-0 before:left-0 before:h-[0.3rem] before:w-24 before:bg-bronze before:content-['']",
           "max-mobile:before:w-16",
-          "max-desktop1024:max-w-[65rem]",
+          "max-desktop1024:max-w-260",
         )}
       >
         {C.title}
@@ -31,8 +31,8 @@ export const Header = (): ReactNode => {
         className={clsx(
           "font-montserrat text-[4rem] font-regular text-dark-blue",
           "max-desktop1024:text-[3.2rem]",
-          "max-mobile:text-[2.4rem] max-mobile:leading-[1.3]",
-          "max-desktop1024:max-w-[65rem]",
+          "max-mobile:max-w-[34ch] max-mobile:text-[2.6rem] max-mobile:leading-normal",
+          "max-desktop1024:max-w-260",
         )}
       >
         {C.description}
@@ -41,7 +41,7 @@ export const Header = (): ReactNode => {
         className={clsx(
           "mt-20 flex gap-[4.8rem]",
           "max-desktop1024:mt-12 max-desktop1024:flex-col max-desktop1024:gap-[2.4rem]",
-          "max-mobile:mt-8 ",
+          "max-mobile:mt-12 max-mobile:gap-[3.2rem]",
         )}
       >
         {C.buttons.map((button) => (
